a07ec39bea083e15f23ba83aff2d54b9d9b11cf8
Jerome Terry <jerome.terry@d2l.com>
2015-04-30 23:59:33 -0230

    Setting default username and password
    I could not get the neo4j authentication extension to work with neo4j
    2.2.1. https://github.com/neo4j-contrib/authentication-extension. Looks
    like the authentication extension doesn't support 2.2.1 (the repo only
    has jar files up to 2.1.5). Using 2.1.5 extension caused neo4j to start,
    then immediately stop.
    
    So instead, copied the contents of the /data/dbms/auth file after
    setting the default neo4j credentials to neo4j / password

--- README.md
+++ README.md
@@ -23,3 +23,8 @@ docker run -itd --name neo4j -p 7474:7474 -p 1337:1337 jterry/neo4j
 
 ### Open neo4j in browser
 http://localhost:7474
+
+### Default Credentials
+Username: neo4j
+
+Password: password
--- launch.sh
+++ launch.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-// credit: https://github.com/tpires/neo4j/blob/master/launch.sh
-
 NEO4J_HOME=/var/lib/neo4j
-
 sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+mkdir $NEO4J_HOME/data/dbms
+echo "neo4j:SHA-256,E218A33CC4291A4972DB27EA09900E204521B8837E6DADC4004131D2C82011A8,8200BAEA75FE46451827B020D7DDC58E:" >> $NEO4J_HOME/data/dbms/auth
 ulimit -n 65536
-
 .$NEO4J_HOME/bin/neo4j console
